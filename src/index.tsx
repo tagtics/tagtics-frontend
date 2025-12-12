4a397bdd25a562ed366ce11abaa1ab7a3e6a484f
tagtics <tagticsdev@gmail.com>
2025-12-12 11:09:13 +0530

    chore: setup tagtics-client package and basic configuration

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import './index.css';
 import { createRoot } from 'react-dom/client';
 import { AppRouter } from './AppRouter';
+import Tagtics from 'tagtics-client'; // Import it
+
+// Initialize Tagtics here
+Tagtics.init({
+  apiKey: 'TEST_KEY',
+  excludePaths: ['.*'],  // Show everywhere
+  privacyNotice: 'Custom privacy notice for testing',
+  serializeChildDepth: 2,
+  allowSensitivePages: true,  // Test on all pages
+});
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
