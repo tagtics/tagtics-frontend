29369ea41339615954d29451cb963e6c87fce40e
tagtics <tagticsdev@gmail.com>
2025-12-25 08:41:34 +0530

    Enabled test mode for better visibility on tagtics-client package

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export function App() {
             includePaths: ['^/$'],  // Only home page
             privacyNotice: 'Custom privacy notice for testing',
             serializeChildDepth: 2,
-            // testingMode: true,
+            testingMode: true,
         });
 
         return () => {
